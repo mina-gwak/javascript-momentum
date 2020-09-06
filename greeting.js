651c3166b618e0abd57bfbe437eea1adc806e926
mina-gwak <mina.gwak@kakao.com>
2020-09-07 05:34:24 +0900

    Add user name saving

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(name) {
+  localStorage.setItem(USER_LS, name);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(name) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${name}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // she is not
+    askForName();
+  } else {
+    // she is
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
